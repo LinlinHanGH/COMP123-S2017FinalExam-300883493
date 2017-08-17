481643344102ddbc6819cd919302bb56356caed9
Linlin <hanlinlin@live.com>
2017-08-17 10:56:10 -0400

    Start the SplashForm first instead of PickHighestCardForm

--- COMP123-S2017FinalExam-300883493/Program.cs
+++ COMP123-S2017FinalExam-300883493/Program.cs
@@ -8,14 +8,17 @@ using System.Windows.Forms;
  * Name: Linlin Han
  * Date: Aug 17, 2017
  * StudentID: 300883493
- * Description: This is the Splash form class
- * Version: 0.1-Created the Splash form
+ * Description: This is the driver class
+ * Version: 0.2-Start the SplashForm first instead of PickHighestCardForm
  */
 
 namespace COMP123_S2017FinalExam_300883493
 {
     public static class Program
     {
+        //create Reference to Forms
+        public static PickHighestCardForm pickHighestCardForm;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -24,7 +27,9 @@ namespace COMP123_S2017FinalExam_300883493
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PickHighestCardForm());
+
+            pickHighestCardForm = new PickHighestCardForm();
+            Application.Run(new SplashForm());
         }
     }
 }
--- COMP123-S2017FinalExam-300883493/SplashForm.cs
+++ COMP123-S2017FinalExam-300883493/SplashForm.cs
@@ -8,6 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * Name: Linlin Han
+ * Date: Aug 17, 2017
+ * StudentID: 300883493
+ * Description: This is the Splash form class
+ * Version: 0.1-Created the Splash form
+ */
+
 namespace COMP123_S2017FinalExam_300883493
 {
     public partial class SplashForm : Form
@@ -17,6 +25,11 @@ namespace COMP123_S2017FinalExam_300883493
             InitializeComponent();
         }
 
+        /// <summary>
+        /// This is the event handler for the "Tick"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void SplashFormTimer_Tick(object sender, EventArgs e)
         {
             PickHighestCardForm pickHightestCardForm = new PickHighestCardForm();
